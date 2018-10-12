3e4d1abaeedc345fb3d40db4a63d5c5c7a1a232e
Julie Ores <julie.ores@gmail.com>
2018-10-11 23:45:24 -0500

    images not working

--- my-app/src/components/CardContainer.js
+++ my-app/src/components/CardContainer.js
@@ -5,47 +5,49 @@ import './CardContainer.css';
 
 const travelScenes = [
     {
+
         name: "adventure",
-        img: './assets/images/adventure'
+        img: "./assets/images/adventure"
     },
     {
+
         name: "alps",
-        img : './assets/images/alps'
+        img: "./assets/images/alps"
     },
     {
+
         name: "apartment",
-        img : './assets/images/apartment'
+        img: "./assets/images/apartment"
     },
     {
+
         name: "australia",
-        img : './assets/images/australia'
+        img: "./assets/images/australia"
     },
     {
         name: "bay",
-        img : './assets/images/bay'
+        img: './assets/images/bay'
     },
     {
         name: "beach",
-        img : './assets/images/beach'
+        img: './assets/images/beach'
     },
     {
         name: "boat",
-        img : './assets/images/boat'
+        img: "./assets/images/boat"
     },
     {
         name: "camera",
-        img : './assets/images/camera'
+        img: "./assets/images/camera"
     },
     {
         name: "paris",
-        img : './assets/images/paris'
+        img: "./assets/images/paris"
     },
     {
         name: "phonebooth",
-        img : './assets/images/phonebooth'
+        img: "./assets/images/phonebooth"
     },
-
-
 
 
 ]
@@ -56,14 +58,18 @@ class CardContainer extends React.Component {
         this.state = {}
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="cardContainer">
-              {
-                  travelScenes.map(e =>{
-                      return <Card name={e.name} img={e.img}/>
-                  })
-              }
+                {
+                    travelScenes.map(e => {
+                        return <Card name={e.name} img={e.jpg} />
+                
+                        
+                    //    return <Card name={img src={require("./assets/images.alps.jpg") alt="" className="img-responsive" />
+                    })
+                
+                }
             </div>
         )
     }
@@ -71,3 +77,28 @@ class CardContainer extends React.Component {
 
 
 export default CardContainer;
+
+// Testing Image Code
+// var Foo = React.createClass({
+//     render: function() {
+//      const images = ['a','b','c']
+//      const imagesElements = images.map( e => {
+//       return (
+//        <img src={`${e}.jpg`} />
+//       )
+//      });
+  
+//      return(
+//       <div>
+//        {imagesElements}
+//       </div>
+//      )
+//    }
+//   });
+  
+//   ReactDOM.render(
+//    <Foo name="bar" />,
+//    document.getElementById('container')
+//   );
+
+// On clicks for scoring functionality
--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 function Header () {
     return(
         <div className="header">
-            <h1>This is where our header will go</h1>
+            <h1>Scene Selection Clicky Game</h1>
         </div>
     )
 }
